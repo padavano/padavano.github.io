5ee2209451bb09e80eccc42c14d3587fc6403803
padavano <prostotak86@gmail.com>
2025-11-19 15:59:10 +0300

    Update buttons.js

--- buttons.js
+++ buttons.js
@@ -1,82 +1,56 @@
 Lampa.Platform.tv();
-
 (function () {
-    // Params объявляется, но не инициализируется, чтобы избежать ошибки reading 'field' до onStart.
-    let Params = null;
+  "use strict";
 
-    // Регистрация слушателя: прямое обращение к Lampa.Listener.
-    if (Lampa.Listener && typeof Lampa.Listener.follow === 'function') {
-        Lampa.Listener.follow('controller', function (e) {
-            if (e.type === 'look_make_buttons' && e.data && e.data.buttons) {
-                let buttons = e.data.buttons;
-
-                let online = [];
-                let torrent = [];
-                let trailer = [];
-                let other = [];
-
-                for (let i = 0; i < buttons.length; i++) {
-                    let btn = buttons[i];
-                    let name = (btn.name || '').toLowerCase();
-
-                    if (name.indexOf('online') > -1 || name.indexOf('playlist') > -1) {
-                        online.push(btn);
-                    } else if (name.indexOf('torrent') > -1) {
-                        torrent.push(btn);
-                    } else if (name.indexOf('trailer') > -1) {
-                        trailer.push(btn);
-                    } else {
-                        other.push(btn);
-                    }
-                }
-
-                e.data.buttons = [].concat(online, torrent, trailer, other);
-            }
-        });
+  if (Lampa.Storage.get("full_btn_priority") === undefined) {
+    Lampa.Storage.set("full_btn_priority", "{}");
+  }
+  Lampa.Listener.follow("full", function (_0x160bda) {
+    if (_0x160bda.type !== "complite") {
+      return;
     }
-
-    // Регистрация плагина: прямое обращение к Lampa.Plugin и Lampa.Component.
-    if (Lampa.Plugin && Lampa.Component && typeof Lampa.Plugin.create === 'function') {
-        Lampa.Plugin.create({
-            title: 'Sort buttons',
-            id: 'sort_buttons',
-            // Используем Lampa.Component напрямую.
-            component: Lampa.Component.Button, 
-            
-            // onStart гарантирует, что Storage доступен для инициализации Params.
-            onStart: function () {
-                if (Lampa.Storage) {
-                    Params = Lampa.Storage.field('full_btn_priority', '');
-                    Params.value = '';
-                }
-            },
-            
-            onStop: function () {
-                if (Params) {
-                    Params.value = '';
-                }
-            },
-            
-            settings: function () {
-                let field = {
-                    name: 'full_btn_priority',
-                    title: 'Default button priority',
-                    type: 'select',
-                    value: Params ? Params.value : '', 
-                };
-
-                field.onSelect = function (data) {
-                    if (Params) {
-                        Params.value = data.value;
-                    }
-                    if (Lampa.Storage) {
-                        Lampa.Storage.set('full_btn_priority', data.value);
-                    }
-                };
-
-                return [field];
-            }
+    setTimeout(function () {
+      var _0x4be01c = _0x160bda.object.activity.render();
+      if (!_0x4be01c.length) {
+        return;
+      }
+      var _0x2b8f31 = _0x4be01c.find(".full-start-new__buttons");
+      if (!_0x2b8f31.length) {
+        return;
+      }
+      _0x4be01c.find(".button--play").remove();
+      var _0x15115e = _0x4be01c.find(".buttons--container .full-start__button").add(_0x2b8f31.find(".full-start__button"));
+      var _0x14389c = {
+        online: [],
+        torrent: [],
+        trailer: [],
+        other: []
+      };
+      _0x15115e.each(function (_0x5ee25e) {
+        var _0x5e0019 = $(this);
+        var _0x247ddc = _0x5e0019.attr("class") || "";
+        var _0x2afbda = _0x5e0019.clone(true).data("index", _0x5ee25e);
+        _0x14389c[_0x247ddc.includes("online") ? "online" : _0x247ddc.includes("torrent") ? "torrent" : _0x247ddc.includes("trailer") ? "trailer" : "other"].push(_0x2afbda);
+      });
+      _0x2b8f31.empty();
+      ["online", "torrent", "trailer", "other"].forEach(function (_0x40b4ad) {
+        _0x14389c[_0x40b4ad].sort(function (_0x519ace, _0x440d50) {
+          return $(_0x519ace).data("index") - $(_0x440d50).data("index");
         });
-    }
-
+        _0x14389c[_0x40b4ad].forEach(function (_0x47dc11) {
+          _0x2b8f31.append(_0x47dc11);
+        });
+      });
+      _0x2b8f31.css({
+        display: "flex",
+        flexWrap: "wrap",
+        gap: "10px"
+      });
+      var _0x3cf5f9 = "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 32 32\"><path d=\"M20.331 14.644l-13.794-13.831 17.55 10.075zM2.938 0c-0.813 0.425-1.356 1.2-1.356 2.206v27.581c0 1.006 0.544 1.781 1.356 2.206l16.038-16zM29.512 14.1l-3.681-2.131-4.106 4.031 4.106 4.031 3.756-2.131c1.125-0.893 1.125-2.906-0.075-3.8zM6.538 31.188l17.55-10.075-3.756-3.756z\" fill=\"currentColor\"></path></svg>";
+      _0x2b8f31.find(".lampac--button").each(function () {
+        $(this).find("svg").remove().end().prepend(_0x3cf5f9);
+      });
+      Lampa.Controller.toggle("full_start");
+    }, 100);
+  });
 })();
